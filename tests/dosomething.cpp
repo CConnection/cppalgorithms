60fd2547ea582b394a55b002cf0c92cd24afeefc
Thomas Kunze <thomas.s.kunze@me.com>
2020-06-06 20:05:38 +0200

    Added project

---
+++ tests/dosomething.cpp
@@ -0,0 +1,5 @@
+//
+// Created by Thomas Kunze on 06.06.20.
+//
+
+#include "dosomething.h"
---
+++ tests/dosomething.h
@@ -0,0 +1,12 @@
+//
+// Created by Thomas Kunze on 06.06.20.
+//
+
+#ifndef ALGORITHMS_TESTS_DOSOMETHING_H
+#define ALGORITHMS_TESTS_DOSOMETHING_H
+
+class dosomething {
+
+};
+
+#endif //ALGORITHMS_TESTS_DOSOMETHING_H
